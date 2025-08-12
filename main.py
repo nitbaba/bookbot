897cd2f8d7d05e94c618a5481e46196e84906d2b
nitbaba <nitbaba@gmail.com>
2025-08-11 20:10:10 -0400

    "Added to gitignore, created count of unique char func"

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+from stats import *
+
 def get_book_text(book_filepath):
     with open(book_filepath, 'r') as f:
         book_contents = f.read()
     return book_contents
 
-def num_words_in_book(book):
-    book_as_list = book.split()
-    return len(book_as_list)
-
 def main(filepath):
     book_text = get_book_text(filepath)
     num_words = num_words_in_book(book_text)
     print(f"{num_words} words found in the document")
+    char_count = char_count_in_book(book_text)
+    print(f"{char_count}")
 
 main('./books/frankenstein.txt')
